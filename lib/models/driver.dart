943e9b8cc15bd0d7d75517938c70fe9da329b901
ikesh pack <packappsforyou@gmail.com>
2019-10-27 13:51:44 -0400

    Add getter for avg speed of a driver

--- lib/models/driver.dart
+++ lib/models/driver.dart
@@ -7,11 +7,17 @@ class Driver {
   List<Trip> trips = [];
   int get distanceTraveled => tallyDistances();
   int get timeTaken => tallyTimes();
-  int avgSpeed = 0;
+  int get avgSpeed => (tallyDistances() / tallyTimes()).round();
 
   int tallyDistances() {
     double total = 0;
     trips.forEach((Trip trip) => total += trip.distance);
     return total.round();
+  }
+
+  int tallyTimes() {
+    int total = 0;
+    trips.forEach((Trip trip) => total += (trip.endTime = trip.startTime));
+    return total;
   }
 }
