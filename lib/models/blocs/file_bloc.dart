ac2ac9e396f9ec6185a59b6d3b8a109f02242e90
ikesh pack <packappsforyou@gmail.com>
2019-10-28 11:45:20 -0400

    Add basic structure to FileBloc

--- lib/models/blocs/file_bloc.dart
+++ lib/models/blocs/file_bloc.dart
@@ -1,1 +1,16 @@
-class FileBloc {}
+import 'package:rxdart/subjects.dart';
+import 'package:trip_tracker/models/events/file_event.dart';
+
+export 'package:trip_tracker/models/events/file_event.dart';
+
+class FileBloc {
+  final eventSubject = BehaviorSubject<FileEvent>();
+
+  FileBloc() {
+    eventSubject.listen((FileEvent event) {});
+  }
+
+  dispose() {
+    eventSubject?.drain();
+  }
+}
