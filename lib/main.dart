77dfcc0cfe536d36b32bda6669675aff22d97231
ikesh pack <packappsforyou@gmail.com>
2019-10-28 11:55:15 -0400

    Add provider as top of the widget tree

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:trip_tracker/blocs/file_bloc.dart';
 import 'package:trip_tracker/views/home.dart';
 
 main() => runApp(
-      MaterialApp(
-        title: 'Trip Tracker',
-        home: MyHomePage(),
+      Provider(
+        builder: (BuildContext context) => FileBloc(),
+        dispose: (BuildContext context, FileBloc fileBloc) =>
+            fileBloc.dispose(),
+        child: MaterialApp(
+          title: 'Trip Tracker',
+          home: MyHomePage(),
+        ),
       ),
     );
