36330bdeb1081fc959db79f584fccb9e05435d93
ikesh pack <packappsforyou@gmail.com>
2019-10-27 20:45:10 -0400

    Add helpful comments to trip.dart

--- lib/models/trip.dart
+++ lib/models/trip.dart
@@ -1,8 +1,13 @@
 class Trip {
+  // contstructor function.
   Trip({this.driver, this.startTime, this.endTime, this.distance});
 
+  // name of driver that took the trip.
   String driver;
+  // start time of trip in milliseconds since epoch.
   int startTime;
+  // end time of trip in milliseconds since epoch.
   int endTime;
+  // recorded distance of trip.
   double distance;
 }
