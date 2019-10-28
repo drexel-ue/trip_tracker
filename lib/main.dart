b1ec97557855cd7bbb2850e53ad2995134a1936b
ikesh pack <packappsforyou@gmail.com>
2019-10-28 11:51:21 -0400

    Furthur restructuring to follow convention

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:trip_tracker/views/home.dart';
 
-main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Trip Tracker',
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color.fromRGBO(72, 90, 119, 1),
-      appBar: AppBar(
-        title: const Text('Trip Tracker'),
-        backgroundColor: const Color.fromRGBO(72, 90, 119, 1),
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.only(top: 20),
-          child: Column(
-            children: <Widget>[
-              const Text(
-                'Select Input File',
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 10.0),
-                child: const Text(
-                  'ex: selected_file.txt',
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                    decoration: TextDecoration.underline,
-                    decorationStyle: TextDecorationStyle.solid,
-                    decorationColor: Colors.white,
-                  ),
-                ),
-              ),
-              FlatButton(
-                onPressed: () {},
-                color: Colors.white,
-                child: Text(
-                  'SELECT FILE',
-                  style: TextStyle(
-                    color: const Color.fromRGBO(72, 90, 119, 1),
-                    fontSize: 18,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
+main() => runApp(
+      MaterialApp(
+        title: 'Trip Tracker',
+        home: MyHomePage(),
       ),
     );
-  }
-}
---
+++ lib/views/home.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(72, 90, 119, 1),
+      appBar: AppBar(
+        title: const Text('Trip Tracker'),
+        backgroundColor: const Color.fromRGBO(72, 90, 119, 1),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 20),
+          child: Column(
+            children: <Widget>[
+              const Text(
+                'Select Input File',
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
+                child: const Text(
+                  'ex: selected_file.txt',
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    decoration: TextDecoration.underline,
+                    decorationStyle: TextDecorationStyle.solid,
+                    decorationColor: Colors.white,
+                  ),
+                ),
+              ),
+              FlatButton(
+                onPressed: () {},
+                color: Colors.white,
+                child: Text(
+                  'SELECT FILE',
+                  style: TextStyle(
+                    color: const Color.fromRGBO(72, 90, 119, 1),
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
