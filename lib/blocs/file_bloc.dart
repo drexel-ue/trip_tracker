385ba1fae26cefbe9f0b1d7b69c0e0acb2c5674e
ikesh pack <packappsforyou@gmail.com>
2019-10-28 11:46:19 -0400

    Correct file structure

--- lib/blocs/file_bloc.dart
+++ lib/blocs/file_bloc.dart
@@ -1,7 +1,7 @@
 import 'package:rxdart/subjects.dart';
-import 'package:trip_tracker/models/events/file_event.dart';
+import 'package:trip_tracker/events/file_event.dart';
 
-export 'package:trip_tracker/models/events/file_event.dart';
+export 'package:trip_tracker/events/file_event.dart';
 
 class FileBloc {
   final eventSubject = BehaviorSubject<FileEvent>();
